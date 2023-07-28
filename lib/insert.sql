ec1df37d428bb827d39e22f962e2578e27994b40
Steve Romero <romero.steve.a@gmail.com>
2023-07-28 14:10:02 -0700

    bug fix

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'asdf', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'brown', 'asdf', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'brown', 'asdf', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'brown', 'asdf', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'M', 'brown', 'asdf', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'brown', 'asdf', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'M', 'brown', 'asdf', FALSE);
-INSERT INTO bears (age, sex, color, temperament, alive) VALUES (10, 'M', 'brown', 'asdf', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'asdf', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'brown', 'asdf', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'brown', 'asdf', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'brown', 'asdf', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'M', 'brown', 'asdf', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'brown', 'asdf', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'M', 'brown', 'asdf', 0);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (10, 'M', 'brown', 'asdf', 1);
 
